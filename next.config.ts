1b5d14cdfa74be2e0b8d8803a0b5e689743f6728
Peter Moss <petermoss@Peters-MacBook.local>
2026-04-23 10:31:50 +0800

    fix: next config for webpack build

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  experimental: {
-    turbo: undefined
-  }
+  bundlePagesRouterDependencies: true,
 }
 
 export default nextConfig
